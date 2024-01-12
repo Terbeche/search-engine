eca819e651b04bef27ac1e60a2f3a17ef0e9a017
Mostefa-Terbeche <termus96@gmail.com>
2024-01-12 22:14:42 +0100

    Add dummy data for test purpose

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,14 @@
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
+# db/seeds.rb
+
+# Array of dummy search terms
+search_terms = ['test', 'hello', 'world', 'rails', 'ruby', 'programming']
+
+# Array of dummy IP addresses
+ip_addresses = ['192.0.2.1', '192.0.2.2', '192.0.2.3', '192.0.2.4', '192.0.2.5']
+
+# Create dummy records
+ip_addresses.each do |ip|
+  search_terms.each do |term|
+    Search.create(term: term, ip: ip)
+  end
+end
